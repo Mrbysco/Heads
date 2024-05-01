6a1c67c3b0e0619b24f56d5fb07ad7e05f2a5299
Mrbysco <Mrbysco@users.noreply.github.com>
2024-05-01 16:39:46 +0200

    Remove unused imports

--- src/main/java/com/mrbysco/heads/config/HeadConfig.java
+++ src/main/java/com/mrbysco/heads/config/HeadConfig.java
@@ -1,9 +1,9 @@
 package com.mrbysco.heads.config;
 
 import com.mrbysco.heads.Heads;
-import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class HeadConfig {

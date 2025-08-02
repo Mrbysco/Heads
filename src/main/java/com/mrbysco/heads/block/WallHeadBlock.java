612c70648bff42db19970377588a4d715617e594
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-02 22:37:42 +0200

    Update WallHeadBlock.java

--- src/main/java/com/mrbysco/heads/block/WallHeadBlock.java
+++ src/main/java/com/mrbysco/heads/block/WallHeadBlock.java
@@ -67,6 +67,7 @@ public class WallHeadBlock extends WallSkullBlock {
 		return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
